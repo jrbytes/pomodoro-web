129022bb86d56225405e4c305c4c5e232f29aa97
Jr Bytes <itamazor@gmail.com>
2020-07-31 22:46:06 -0300

    :tada: Initial commit with countdown.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [pomo, setPomo] = useState({})
+  const [countdownValue, setCountdownValue] = useState(0)
+  const [countdownTimeout, setCountdownTimeout] = useState(0)
+  const [button, setButton] = useState(true)
+
+  useEffect(() => {
+    function loadTaskData() {
+      const task = {
+        title: 'Tarefa 1',
+        realizedPomos: 7,
+        defaultMinutesPomo: 25
+      }
+
+      setPomo(task)
+      setCountdownValue(task.defaultMinutesPomo * 60)
+    }
+
+    loadTaskData()
+  }, [])
+
+  const startCountdown = (result) => {
+    setCountdownTimeout(setTimeout(function() {
+      const second = result - 1
+      startCountdown(second)
+      setCountdownValue(second)
+    }, 1000))
+    setButton(false)
+    console.log(result)
+  }
+
+  const pauseCountdown = () => {
+    clearTimeout(countdownTimeout)
+    setButton(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>{pomo.title}</h2>
+      <span>{pomo.realizedPomos}</span>
+      <p>{pomo.defaultMinutesPomo}</p>
+      <p>{countdownValue}</p>
+      {button && <button
+        onClick={() => startCountdown(countdownValue)}
+      >
+        Start
+      </button>}
+      {!button && <button
+        onClick={pauseCountdown}
+      >
+        Pause
+      </button>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
