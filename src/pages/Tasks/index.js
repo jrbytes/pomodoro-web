6b914074c559412a970aeb3697049ac9cadac3fb
Jr Bytes <itamazor@gmail.com>
2020-08-10 20:15:14 -0300

    Added routering

---
+++ src/pages/Tasks/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Tasks = () => {
+  return <h1>Tarefas</h1>
+}
+
+export default Tasks
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Tasks from './pages/Tasks'
+import Pomodoro from './pages/Pomodoro'
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Tasks} />
+      <Route path="/pomodoro" component={Pomodoro} />
+    </BrowserRouter>
+  )
+}
+
+export default Routes
