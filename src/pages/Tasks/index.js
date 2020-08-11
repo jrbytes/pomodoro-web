1f6abe851220b3b76f23c52a10c7611ef00f2fb6
Jr Bytes <itamazor@gmail.com>
2020-08-10 21:53:34 -0300

    Added data in json. Added list tasks

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { IoIosAlarm, IoIosList } from 'react-icons/io'
+
+import './styles.css'
+import tasksData from './data.json'
 
 const Tasks = () => {
-  return <h1>Tarefas</h1>
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    function loadTasks() {
+      setTasks(tasksData)
+    }
+
+    loadTasks()
+  }, [])
+
+  return (
+    <div className="container">
+      <h2>
+        <IoIosList />
+        Tarefas
+      </h2>
+      {tasks.map(item => (
+        <div className="task" key={item.title}>
+          <p>{item.title}</p>
+          <div className="task-pomos">
+            <span>{item.realizedPomos}</span>
+            <IoIosAlarm className="icon" />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Tasks
